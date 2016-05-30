48a9fbb4c1a0c68afd05253abdcf9b96722728f9
Shannon Wu <inatu@icloud.com>
2016-05-30 14:25:50 +0800

    Add UIKit general playground

---
+++ uikitplayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: UIKit Playground
+
+import UIKit
+import XCPlayground
+
+<#code#>
+
+
+//XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+//XCPlaygroundPage.currentPage.liveView = <#live view#>
